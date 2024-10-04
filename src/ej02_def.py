688ac301ae15ed5c9c3a089cf86d48a8bb877924
danielmi5 <midaniel@iesrafaelalberti.es>
2024-10-04 20:53:41 +0200

    1.2d en ejecución 2.0

---
+++ src/ej02_def.py
@@ -0,0 +1,4 @@
+#recibe horas y coste y retorna el importe total.
+def importe_total(horas,coste):
+    return horas*coste
+
---
+++ src/ej04_def.py
@@ -0,0 +1,3 @@
+#NO recibe parámetros y retorna una cadena de caracteres con la temperatura convertida en grados Celsius y entre parántesis la temperatura en grados farenheit... ambas temperaturas con 2 posiciones decimales. Por ejemplo, si introduce 212 debe retornar la cadena "100.00ºC (212.00ºF)". Dentro de la función se pedirá al usuario los grados Farenheit.
+
+def covertir_temp()
