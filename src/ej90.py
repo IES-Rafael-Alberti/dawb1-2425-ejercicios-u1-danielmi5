b208e2172949033d71b12c3000f9b4c3c85e43df
danielmi5 <midaniel@iesrafaelalberti.es>
2024-10-13 16:38:56 +0200

    juego actualizado

---
+++ src/ej90.py
@@ -0,0 +1,3 @@
+import os
+
+os.system('python ej01.py')
--- src/juego_adivinar_num.py
+++ src/juego_adivinar_num.py
@@ -22,44 +22,67 @@ def dificultad(num_dificultad):
   return intentos
     
 def comprobar_entero_dificultad (num_dificultad):
-  if num_dificultad.isdigit() == False:
+  while num_dificultad.isdigit() == False:
+      num_dificultad = input("ERROR: Debe introducir un número (1, 2 o 3): ").strip()
+      while num_dificultad.isdigit() == True:
+        if int(num_dificultad) >= 1 and int(num_dificultad) <=3:
+          return int(num_dificultad) 
+        else:
+          num_dificultad = input("ERROR: Número equivocado (1, 2 o 3): ").strip()
+          if num_dificultad.isdigit() == True and int(num_dificultad) >= 1 and int(num_dificultad) <=3:
+            return int(num_dificultad)
+  while num_dificultad.isdigit() == True:
+    if int(num_dificultad) >= 1 and int(num_dificultad) <=3:
+      return int(num_dificultad) 
+    else:
+      num_dificultad = input("ERROR: Introduce un número dentro del rango (1, 2 o 3): ").strip()
+      if num_dificultad.isdigit() == True and int(num_dificultad) >= 1 and int(num_dificultad) <=3:
+        return int(num_dificultad)    
     while num_dificultad.isdigit() == False:
-      print ("Valor errónea (1, 2 o 3)")
-      num_dificultad = input("Elección: ")
-  if num_dificultad.isdigit() == True:
-    num_dificultad = int(num_dificultad)
-  while num_dificultad <1 or num_dificultad >3:
-      print ("Número equivocado (1, 2 o 3)")
-      num_dificultad = int(input("Elección: "))
+      num_dificultad = input("ERROR: Debe introducir un número (1, 2 o 3): ").strip()
       
   return num_dificultad
   
 def comprobar_entero_num(num):
-  if num.isdigit() == False:
+  while num.isdigit() == False:
+      num = input("ERROR: Debe introducir un número (1-100): ").replace(" ","")
+      while num.isdigit() == True:
+        if int(num) >= 1 and int(num) <=100:
+          return int(num) 
+        else:
+          num = input("ERROR: Introduce un número dentro del rango (1-100): ").replace(" ","")
+          if num.isdigit() == True and int(num) >= 1 and int(num) <=100:
+            return int(num)
+  while num.isdigit() == True:
+    if int(num) >= 1 and int(num) <=100:
+      return int(num) 
+    else:
+      num = input("ERROR: Introduce un número dentro del rango (1-100): ").replace(" ","")
+      if num.isdigit() == True and int(num) >= 1 and int(num) <=100:
+        return int(num)    
     while num.isdigit() == False:
-      print ("**Valor erróneo**  Debe ser un número (1-100)")
-      num = input("Elección: ")
-  if num.isdigit() == True:
-    num = int(num)
-  while num <1 or num >= 100:
-      print ("Número equivocado (1-100)")
-      num = int(input("Elección: "))
-  return num
-      
+      num = input("ERROR: Debe introducir un número (1-100): ").replace(" ","")
+        
   
 
 def comprobar_num (num, random, intentos):
   while (num != random and intentos > 0):
     if num > random:
-      print ("No, el número es menor")
+      if num < random + 10:
+        print ("No, el número es menor (🔥🔥)")
+      else: 
+        print ("No, el número es menor (🧊🧊)")
     if num < random:
-      print ("No, el número es mayor") 
+      if num > random - 10:
+        print ("No, el número es mayor (🔥🔥)")
+      else: 
+        print ("No, el número es mayor (🧊🧊)")
     if intentos > 1:
       print("Te quedan", intentos , "intentos: ", end="")
     else:
       print("Te queda", intentos , "intento: ", end="")
     intentos -= 1
-    num = (input())
+    num = (input()).replace(" ","")
     num = comprobar_entero_num (num)
   
   return num
@@ -71,7 +94,7 @@ def main():
   1: Fácil (10 Intentos)
   2: Normal (5 intentos)
   3: Imposible (1 intento)""")
-  numDif = (input("Elección: "))
+  numDif = (input("Elige dificultad: ")).strip()
   num_dificultad = comprobar_entero_dificultad(numDif)
   intento = dificultad(num_dificultad)
   num = introduce_num ("Adivina el número: ")
@@ -82,6 +105,7 @@ def main():
     print ("Has acertado, enhorabuena!!!")
   else:
     print ("Has fallado 😂😂😂")
+    print ("El número era:" , random)
     
 if __name__ == "__main__":
   main()
