9de64bd143ea81c9f293a35ea852dc04d3a94227
danielmi5 <midaniel@iesrafaelalberti.es>
2024-10-15 09:50:42 +0200

    Los ejercicios terminados en a, son los nuevos del 1.2a. Y los que terminan en c, son los del 1.2c

--- src/ej30a.py
+++ src/ej30a.py
@@ -1,1 +1,41 @@
 #Escribir un programa que determine si un número es primo
+import math
+def comprobar_num(num: str) -> int:
+  if num.isdigit() == True:
+    return int(num)
+  else:
+    while num.isdigit() == False:
+      print ("ERROR")
+      num = input("Debes introducir un número: ").replace(" ","")
+      if num.isdigit() == True:
+        return int(num)
+
+def comprobar_primo(num: int):
+  if num < 2:
+    print ("No es primo")
+  elif num == 2:
+    print ("Es primo")  
+  elif num != 2 and num % 2 == 0:
+    print ("No es primo")
+  else:
+    terminar = False
+    for i in range(3, int(math.sqrt(num)) + 1, 2):
+      if num % i == 0:
+        print ("No es primo")
+        terminar = True
+        break
+    if terminar == False:
+      print ("Es primo")
+    else:
+      pass 
+      
+    
+  
+def main():
+  print ("Introduce un número:")
+  num = input("").replace(" ","")
+  num = comprobar_num(num)
+  num = comprobar_primo(num)
+
+if __name__ == "__main__":
+    main()
---
+++ src/ej32a.py
@@ -0,0 +1,31 @@
+#Calcular la serie de Fibonacci hasta un número dado
+
+def comprobar_num(num: str) -> int:
+  if num.isdigit() == True:
+    return int(num)
+  else:
+    while num.isdigit() == False:
+      print ("ERROR")
+      num = input("Debes introducir un número: ").replace(" ","")
+      if num.isdigit() == True:
+        return int(num)
+
+def fibonacci(num):
+  if num == 0:
+    return 0
+  elif num == 1:
+    return 1
+  else:
+    secuencia = fibonacci (num - 1) + fibonacci (num - 2)
+    return secuencia
+
+def main():
+  print ("Introduce un número:")
+  num = input("").replace(" ","")
+  num = comprobar_num(num)
+  print ("La secuencia de Fibonacci es:",fibonacci(num))
+  
+  
+
+if __name__ == "__main__":
+    main()
