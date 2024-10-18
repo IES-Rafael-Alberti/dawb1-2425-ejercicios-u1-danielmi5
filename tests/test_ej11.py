698c5bd5afbc12ab1cf55ace0510de6176354d75
danielmi5 <midaniel@iesrafaelalberti.es>
2024-10-18 18:22:29 +0200

    Entrega final

--- tests/test_ej11.py
+++ tests/test_ej11.py
@@ -5,9 +5,9 @@ from src.ej11_def import calcula_IVA
 @pytest.mark.parametrize(
     "n, suma",
     [
-        (5,((5*(5+1))/2)),
+        (5,str((5*(5+1))/2)),
         (12,((12*(12+1))/2)), 
-        (23, ((23*(23+1))/2))
+        (23, str((23*(23+1))/2))
     ]
 )
 def test_calcula_params(n, suma):
